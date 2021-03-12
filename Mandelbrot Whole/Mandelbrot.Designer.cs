30feaf3d8bea87e30c802a6596cbf63e8d368227
Mikołaj <mxd1232@gmail.com>
2021-03-12 21:04:09 +0100

    Mandelbrot black/white with simple zooming at the centre

---
+++ Mandelbrot Whole/Mandelbrot.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Mandelbrot_Whole
+{
+    partial class Mandelbrot
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Mandelbrot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 530);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Mandelbrot";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Mandelbrot_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mandelbrot Whole/Mandelbrot.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandelbrot_Whole
+{
+    public partial class Mandelbrot : Form
+    {
+
+        int quality = 100;
+        double zoom = 1;
+
+        double[] XRange = new double[] { 0, 0 };
+        double[] YRange = new double[] { 0, 0 };
+
+        double startingX=0;
+        double startingY=0;
+
+
+        public Mandelbrot()
+        {
+            InitializeComponent();
+        }   
+        private void Mandelbrot_Shown(object sender, EventArgs e)
+        {
+            DrawMandelbrot();
+        }
+        private void DrawMandelbrot()
+        {
+
+            double xMove = XRange[0];
+            double yMove = YRange[0];
+
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+
+
+
+
+
+
+
+
+
+    
+            pictureBox1.Image = bm;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            XRange[0] = e.X;
+            YRange[0] = e.Y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            XRange[1] = e.X;
+            YRange[1] = e.Y;
+            AdjustAspectRatio();
+        }
+
+        
+        private void AdjustAspectRatio()
+        {
+            FixTables();
+            //TODO - just a click is 0/0?
+            var ratio = Math.Abs(XRange[1] - XRange[0]) / Math.Abs(YRange[1] - YRange[0]);
+            var sratio = pictureBox1.Width / pictureBox1.Height;
+            if (sratio > ratio)
+            {
+                var xf = sratio / ratio;
+                XRange[0] *= xf;
+                XRange[1] *= xf;              
+                zoom *= xf;
+            }
+            else
+            {
+                var yf = ratio / sratio;
+                YRange[0] *= yf;
+                YRange[1] *= yf;
+                zoom *= yf;
+            }
+            
+
+            DrawMandelbrot();
+        }
+        //making first field always smaller
+        private void FixTables()
+        {
+            if (XRange[0] > XRange[1])
+            {
+                double temp = XRange[1];
+                XRange[1] = XRange[0];
+                XRange[0] = temp;
+
+            }
+            if (YRange[0] > YRange[1])
+            {
+                double temp = YRange[1];
+                YRange[1] = YRange[0];
+                YRange[0] = temp;
+
+            }
+        }
+        private void SetWindow()
+        { 
+            if(startingX==0)
+            {
+                startingX = XRange[0]-pictureBox1.Width/2;
+            }
+            else
+            {
+
+            }
+            if (startingY == 0)
+            {
+                startingY = YRange[0] - pictureBox1.Height / 2;
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+    }
+}
+
+
+/*      for (int x = 0; x < pictureBox1.Width; x++)
+          {
+              for (int y = 0; y < pictureBox1.Height; y++)
+              {
+                  double a = (double)(xMove + x - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width/4 ) / zoom;
+                  double b = (double)(yMove + y - (pictureBox1.Height / 2)) / (double)(pictureBox1.Height/4) / zoom;
+
+                  Complex c = new Complex { A = a, B = b };
+                  Complex z = new Complex { A = 0, B = 0 };
+
+                  int i = 0;
+                  do
+                  {
+                      i++;
+                      z.Square();
+                      z.Add(c);
+                      if (z.Magnitude() > 2.0) break;
+                  }
+                  while (i < quality);
+                  bm.SetPixel(x, y, i < quality ? Color.FromArgb(20, 20, i) : Color.Black);
+              }
+          }*/
