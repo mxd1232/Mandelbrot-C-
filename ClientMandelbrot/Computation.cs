e84ecf0a26cdeef1c0eeb0d7e9b14ae41d457408
Mikołaj <mxd1232@gmail.com>
2021-05-29 21:39:35 +0200

    Server Computation refactoring

--- ClientMandelbrot/Computation.cs
+++ ClientMandelbrot/Computation.cs
@@ -16,6 +16,8 @@ namespace ClientMandelbrot
 
         int WidthPixel = 700;
         int HeightPixel = 700;
+
+        double Increment;
   
         public Computation()
         {
@@ -34,68 +36,71 @@ namespace ClientMandelbrot
 
             Bitmap bitmap = new Bitmap(WidthPixel, HeightPixel);
 
-            double increment = 4 / zoom / Diameter();
+            CountIncrement();
 
-
-            for (int xPixel = 0; xPixel < WidthPixel; xPixel++)
-            {
-                List<int> PixelList = new List<int>();
-
-                for (int yPixel = 0; yPixel < HeightPixel; yPixel++)
-                {
-
-                    Complex c = Constant(increment, xPixel, yPixel);
-                    Complex z = new Complex { A = 0, B = 0 };
-
-                    int i = 0;
-                    do
-                    {
-                        i++;
-                        z.Square();
-                        z.Add(c);
-                        if (z.Magnitude() > 2.0) break;
-                    }
-                    while (i < maxIterations);
-                    bitmap.SetPixel(xPixel, yPixel, i < maxIterations ? Color.FromArgb(20, 20, i % 255) : Color.Black);
-                  
-                }
-               
-            }
+            SetAllPixelsColor(bitmap);
+            
             return bitmap;
         }    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public Complex Constant(double increment, int x, int y)
+        public void SetAllPixelsColor(Bitmap bitmap)
         {
-            if (increment == 0)
+            for (int xPixel = 0; xPixel < WidthPixel; xPixel++)
             {
-                increment = 4 / zoom / Diameter();
+                for (int yPixel = 0; yPixel < HeightPixel; yPixel++)
+                {
+                    SetPixelColor(bitmap, xPixel, yPixel);
+                }
+            }
+        }
+
+
+        public void SetPixelColor(Bitmap bitmap, int xPixel,int yPixel)
+        {
+            int i = CountIterations(xPixel, yPixel);
+            bitmap.SetPixel(xPixel, yPixel, i < maxIterations ? Color.FromArgb(20, 20, i % 255) : Color.Black);
+        }
+        public int CountIterations(int xPixel,int yPixel)
+        {
+            Complex c = Constant(xPixel, yPixel);
+            Complex z = new Complex { A = 0, B = 0 };
+
+
+            int i = 0;
+            do
+            {
+                i++;
+                z.Square();
+                z.Add(c);
+                if (Math.Pow(z.Magnitude(), 2) >4.0) break;
+            }
+            while (i < maxIterations);
+
+            return i;
+        }
+  
+
+        public Complex Constant(int x, int y)
+        {
+            if (Increment == 0)
+            {
+                 CountIncrement();
             }
 
 
             Complex c = new Complex
             {
-                A = (-increment * WidthPixel / 2 + center[0] + increment * x),
-                B = (increment * HeightPixel / 2 + center[1] - increment * y)
+                A = (-Increment * WidthPixel / 2 + center[0] + Increment * x),
+                B = (Increment * HeightPixel / 2 + center[1] - Increment * y)
             };
             return c;
         }
 
         //making first field always smaller
+        public void CountIncrement()
+        {
+            Increment = 4 / zoom / Diameter();
+        }
+
         public double Diameter()
         {
             return Math.Min(HeightPixel, WidthPixel);
